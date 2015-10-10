454f00b0c47e5733b95e94cd8d2ae3c1d1d3ad77
Lafree317 <lafree317@163.com>
2015-10-10 14:46:13 +0800

    检测网络demo
    检测网络demo

---
+++ Podfile
@@ -0,0 +1,3 @@
+pod ’SDWebImage’,’~>3.7.3'
+pod ’AFNetworking’,’~>2.5.0'
+pod 'MJRefresh', '~> 2.4.11'
--- 检测网络demo/ViewController.m
+++ 检测网络demo/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "AFNetworking.h"
 
 @interface ViewController ()
 
@@ -18,7 +19,44 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
-
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    //创建网络监听管理者对象
+    AFNetworkReachabilityManager *manager = [AFNetworkReachabilityManager sharedManager];
+    
+    /*
+     typedef NS_ENUM(NSInteger, AFNetworkReachabilityStatus) {
+     AFNetworkReachabilityStatusUnknown          = -1,//未识别的网络
+     AFNetworkReachabilityStatusNotReachable     = 0,//不可达的网络(未连接)
+     AFNetworkReachabilityStatusReachableViaWWAN = 1,//2G,3G,4G...
+     AFNetworkReachabilityStatusReachableViaWiFi = 2,//wifi网络
+     };
+     */
+    //设置监听
+    [manager setReachabilityStatusChangeBlock:^(AFNetworkReachabilityStatus status) {
+        switch (status) {
+            case AFNetworkReachabilityStatusUnknown:
+                NSLog(@"未识别的网络");
+                break;
+                
+            case AFNetworkReachabilityStatusNotReachable:
+                NSLog(@"不可达的网络(未连接)");
+                break;
+                
+            case AFNetworkReachabilityStatusReachableViaWWAN:
+                NSLog(@"2G,3G,4G...的网络");
+                break;
+                
+            case AFNetworkReachabilityStatusReachableViaWiFi:
+                NSLog(@"wifi的网络");
+                break;
+            default:
+                break;
+        }
+    }];
+    //开始监听
+    [manager startMonitoring];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
